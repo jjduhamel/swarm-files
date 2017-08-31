f0fae21d73103834dcf48232d2d00168c394fd53
John Duhamel <john.duhamel@gmail.com>
2017-08-30 17:19:09 -0700

    Portainer

---
+++ portainer/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "hbpcb-terraform"
+    key = "portainer/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+  config {
+    bucket = "hbpcb-terraform"
+    key    = "dns/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_route53_record" "apache" {
+  zone_id = "${ data.terraform_remote_state.dns.private_zone }"
+  name = "portainer.homebrewpcb.com"
+  type = "CNAME"
+  ttl = "300"
+  records = [ "swarm.homebrewpcb.com" ]
+}
